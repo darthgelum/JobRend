817d339041c326568742237f52f25cd989f9608a
Philipp Dmitrov <dmitrov.philipp@yandex.ru>
2020-05-22 16:04:33 +0300

    bresenheim line

--- JobRend/MainCanvas.cpp
+++ JobRend/MainCanvas.cpp
@@ -20,45 +20,44 @@ void MainCanvas::DrawLine(Vector3 vertex1, Vector3 vertex2, Uint32 color)
 {
     int scalingFactor = 18;
     // Coord + centerization (center on x, 2/3 on y)
-    int x1 = (vertex1.x * scalingFactor) + GetWidth() * 1 / 2;
-    int y1 = (-vertex1.y * scalingFactor) + GetHeight() * 2 / 3;
-    int x2 = (vertex2.x * scalingFactor) + GetWidth() * 1 / 2;
-    int y2 = (-vertex2.y * scalingFactor) + GetHeight() * 2 / 3;
+    int x0 = (vertex1.x * scalingFactor) + GetWidth() * 1 / 2;
+    int y0 = (-vertex1.y * scalingFactor) + GetHeight();
+    int x1 = (vertex2.x * scalingFactor) + GetWidth() * 1 / 2;
+    int y1 = (-vertex2.y * scalingFactor) + GetHeight();
+    // Bresenheim algo
+    const int deltaX = abs(x1 - x0);
+    const int deltaY = abs(y1 - y0);
+    const int signX = x0 < x1 ? 1 : -1;
+    const int signY = y0 < y1 ? 1 : -1;
 
-    bool steep = false;
-    if (std::abs(x1 - x2) < std::abs(y1 - y2)) {
-        std::swap(x1, y1);
-        std::swap(x2, y2);
-        steep = true;
-    }
-
-    //Redefine line so that it is left to right
-    if (x1 > x2) {
-        std::swap(x1, x2);
-        std::swap(y1, y2);
-    }
-    int dx = x2 - x1;
-    int dy = y2 - y1;
-    int derror2 = std::abs(dy) * 2;
+    int error = 1;
     int error2 = 0;
-    int y = y1;
-    for (int x = x1; x <= x2; x++) {
-        if (steep) {
-            SetPixelColor(color, y, x);
+    //last point
+    SetPixelColor(color,x1, y1);
+    while (x0 != x1 || y0 != y1)
+    {
+        SetPixelColor(color, x0, y0);
+        error2 = error * 2;
+        //
+        if (error2 > -deltaY)
+        {
+            error -= deltaY;
+            x0 += signX;
         }
-        else {
-            SetPixelColor(color, x, y);
-        }
-        error2 += derror2;
-        if (error2 > dx) {
-            y += (y2 > y1 ? 1 : -1);
-            error2 -= dx * 2;
+        if (error2 < deltaX)
+        {
+            error += deltaX;
+            y0 += signY;
         }
     }
 }
 
 Uint32* MainCanvas::SetPixelColor(Uint32 color, int x, int y)
 {
+    if (x < 0 || y < 0 || x>GetWidth() || y>GetHeight())
+    {
+        return &gBuffer[0];
+    }
     int pixelNumber = convertCoordinates(x, y);
     gBuffer[pixelNumber] = color;
     return &gBuffer[pixelNumber];
